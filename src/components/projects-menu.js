0a12df38e48f1c1bb2308b72d0578a7f91f863fd
Vinicius Ribeiro <portov394@gmail.com>
2024-02-13 14:30:32 -0300

    inicio adição de conteudo

--- src/components/projects-menu.js
+++ src/components/projects-menu.js
@@ -1,7 +1,8 @@
 import './projects-menu.css'
 import backgroundFE from './media/responsive-2044921_1280.jpg'
-import backgroundBE from './media/dean-pugh-C8NDn4xk9zs-unsplash.jpg'
-
+import backgroundBE from './media/laptop-1839876_1280.jpg'
+import backgroundAuto from './media/Imagem1.jpg'
+import backgroundOutros from './media/idea-2558678_1280.jpg'
 function Projects(projects) {
   return (
     <>
@@ -10,8 +11,8 @@ function Projects(projects) {
         <div className='porjects' style={{backgroundImage:"url("+backgroundFE+")"}}>
           <a href="/front_end"> <p>Front end</p></a></div>
         <div>
-            <div><a href='/automacao'> <p>Automação</p></a></div>
-            <div><a href="/outros"> <p>Outros</p></a></div>
+            <div style={{backgroundImage:"url("+backgroundAuto+")"}}><a href='/automacao'> <p>Automação</p></a></div>
+            <div style={{backgroundImage:"url("+backgroundOutros+")"}}><a href="/outros"> <p>Outros</p></a></div>
         </div>
         <div style={{backgroundImage:"url("+backgroundBE+")"}}><a href="/back_end" > <p>Back end</p></a></div>
     </section>
